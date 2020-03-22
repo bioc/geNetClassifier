e79b40599d3987ef5af1bb58a953d4ccfd865a16
Sara Aibar Santos <u0103631@gbw-l-m0361.local>
2020-03-22 10:41:06 +0100

    Solving error in plotNetwork

--- R/functions.public.R
+++ R/functions.public.R
@@ -1288,7 +1288,7 @@ plotNetwork  <- function(genesNetwork, classificationGenes=NULL, genesRanking=NU
 
         # Check classificationGenes and Genes ranking format and EXTRACT its genes INFO.
         if(is.matrix(classificationGenes) && nrow(classificationGenes)==0) classificationGenes<-NULL
-        if(class(classificationGenes) == "GenesRanking" && numGenes(classificationGenes) == 0)  classificationGenes <- NULL
+        if(all(class(classificationGenes) == "GenesRanking") && all(numGenes(classificationGenes) == 0))  classificationGenes <- NULL
         if(plotClassifcationGenesNetwork && is.null(classificationGenes)) warning("The classifcation genes network can only be plotted if the classification genes are provided.")
         if((!is.null(classificationGenes) && !is.null(genesRanking)) && !is.null(genesInfo)) stop("Please, provide either 'genesInfo' OR a genesRanking and classificationGenes.")
         if(!is.null(genesRanking) || !is.null(classificationGenes))
